57061ba8b61df8a6515d16a51254fd75765eb349
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-07-05 00:30:57 +0600

    add Welcome.js in src for class props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hello from './my-component/Hello';
+import Welcome from './my-component/Welcome';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Hello name="Elora Khatun" age="20"></Hello>
       <Hello name="Asha Akter" age="15"></Hello>
       <Hello name="Taslima Akter" age="22"></Hello>
+
+      <Welcome name="class"></Welcome>
+      <Welcome name="i am class "></Welcome>
     </div>
   );
 }
